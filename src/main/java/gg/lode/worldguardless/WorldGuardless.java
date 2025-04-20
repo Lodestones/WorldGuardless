fbe14af7a4a634ca982e454291dfa0aa10f4a1fa
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-04-20 04:23:46 -0700

    pom!: Update to Java 17

--- src/main/java/gg/lode/worldguardless/WorldGuardless.java
+++ src/main/java/gg/lode/worldguardless/WorldGuardless.java
@@ -5,7 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import to.lodestone.bookshelfapi.api.util.Metrics;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
